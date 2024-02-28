d143fdb52380935750861981d0ff5880b0c7cb3a
Renaky <emersonrenaki0@gmail.com>
2024-02-28 12:21:06 -0300

    First rest API Tonnie

---
+++ my-first-web-api/src/main/java/dio/myfirstwebapi/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package dio.myfirstwebapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class WelcomeController {
+	@GetMapping
+	public String welcome() {
+		return "Welcome to my springBoot web api";
+	}
+
+}
